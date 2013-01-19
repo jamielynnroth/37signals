03e2e6e3a7881cde39fb00fcc26804d52af61144
Jamie Roth <roth.j@me.com>
2013-01-18 19:29:58 -0500

    Add jquery effects to #collab.cards

---
+++ javascript.js
@@ -0,0 +1,46 @@
+
+
+$(document).ready(function(){
+	$('div#basecamp').hover(
+		function(){
+		$('div#title_default').hide();
+		$('div#title_basecamp').show();},
+
+		function(){
+		$('div#title_default').show();
+		$('div#title_basecamp').hide();
+}
+);
+});
+
+$(document).ready(function(){
+	$('div#highrise').hover(
+		function(){
+		$('div#title_default').hide();
+		$('div#title_highrise').show();},
+
+		function(){
+		$('div#title_default').show();
+		$('div#title_highrise').hide();
+}
+);
+});
+
+$(document).ready(function(){
+	$('div#campfire').hover(
+		function(){
+		$('div#title_default').hide();
+		$('div#title_campfire').show();},
+
+		function(){
+		$('div#title_default').show();
+		$('div#title_campfire').hide();
+}
+);
+});
+
+
+
+
+
+
